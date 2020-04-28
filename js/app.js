7c68fcd0e4dbec74451ffc5d835ec56507335b1e
MichaelRefvem <mkrefvem@gmail.com>
2020-04-28 16:22:48 -0700

    totally set up my js code

---
+++ js/app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// get user name and greeting
+
+var userName = prompt('Hello! What is your name?');
+
+// console.log('the user name is ' + userName);
+
+alert('Hello ' + userName + '! Welcome to the site. Please take a moment to answer a few important questions. Please answer yes or no only.');
+
+// question 1 Is cheese delicious?
+
+var cheese = prompt('Is cheese delicious');
+
+if(cheese.toLowerCase() === 'yes'){
+  alert('You\'ve probably enjoyed some of the best cheeses');
+} else if(cheese.toLowerCase() === 'no'){
+  alert('That\'s too bad. My guess is that you had a bad experience with cheese once.');
+} else {
+  alert('Please answer with a yes or no');
+}
+
+// question 2 Do you like hiking?
+
+var hiker = prompt('Do you enjoy hiking?');
+
+if(hiker.toLowerCase() === 'yes'){
+  alert('Me too! I\'ve been to all sorts of cool places in the Seattle area.');
+} else if(hiker.toLowerCase() === 'no'){
+  alert('Its a great hobby and spending time in nature has so many benefits. You should try it sometime!');
+} else {
+  alert('Please answer with a yes or no');
+}
+
+// question 3 cat person?
+
+var catPerson = prompt('Are you a cat person?');
+
+if(catPerson.toLowerCase() === 'yes'){
+  alert('Nice! So am I, but I don\'t have one yet.');
+} else if(catPerson.toLowerCase() === 'no'){
+  alert('Thats fine. Lots of dog fans out there.');
+} else {
+  prompt('Please answer with a yes or no');
+}
+
+// question 4 meaning of life?
+
+var philosphy = prompt('Does life have a higher meaning?');
+
+if(philosphy.toLowerCase() === 'yes'){
+  alert('Nice to meet a fellow Philosopher!');
+} else if(philosphy.toLowerCase() === 'no'){
+  alert('Monty Python addresses all of this in The Meaning of Life (1983)');
+} else {
+  prompt('Please answer with a yes or no');
+}
+
+// question 5 Tiger King?
+
+var watchedTigerKing = prompt('Did you watch Tiger King on Netflix?');
+
+if(watchedTigerKing.toLowerCase() === 'yes'){
+  alert('That was a crazy show! Wasn\'t it?');
+} else if(watchedTigerKing.toLowerCase() === 'no'){
+  alert('You gotta see it!');
+} else {
+  prompt('Please answer with a yes or no');
+}
+
+alert('Thank you so much for answering the questions ' + userName + '! It was nice to meet you. Continue now to learn a little bit about me on the rest of the site.');
