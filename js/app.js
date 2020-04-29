eb98e129b74537aa05c9c04c0d7799c897c385a2
MichaelRefvem <mkrefvem@gmail.com>
2020-04-29 16:21:31 -0700

    new JS for lab 3

--- js/app.js
+++ js/app.js
@@ -13,9 +13,9 @@ alert('Hello ' + userName + '! Welcome to the site. Please take a moment to answ
 var cheese = prompt('Is cheese delicious');
 
 if(cheese.toLowerCase() === 'yes'){
-  alert('You\'ve probably enjoyed some of the best cheeses');
+  alert('You\'ve probably enjoyed some of the best cheeses!');
 } else if(cheese.toLowerCase() === 'no'){
-  alert('That\'s too bad. My guess is that you had a bad experience with cheese once.');
+  alert('That\'s too bad. My guess is that you must have had a bad experience with cheese.');
 } else {
   alert('Please answer with a yes or no');
 }
@@ -61,21 +61,56 @@ if(philosphy.toLowerCase() === 'yes'){
 var playInstrument = prompt('Do you play any musical instruments?');
 
 if(playInstrument.toLowerCase() === 'yes'){
-  alert('That\'s so cool! I\'ve played the piano for 25 years.);
+  alert('That\'s so cool! I\'ve played the piano for 25 years.');
 } else if(playInstrument.toLowerCase() === 'no'){
   alert('You\'re never too old to start learning!');
 } else {
   prompt('Please answer with a yes or no');
 }
 
-alert('Thank you so much for answering the questions ' + userName + '! It was nice to meet you. Continue now to learn a little bit about me on the rest of the site.');
 
 // question 6 Number guessing game
 // Guess a number between 1 and 20 (answer is 12)
 
 var correctAnswer = 12
 for(var i = 0; i < 4; i = i + 1){
-  var numberGuess = prompt('Please')
+  var numberGuess = prompt('Now I would like to play a little number game. Please guess a number between 1 and 20. You have four guesses!')
+  if (numberGuess == correctAnswer) {
+    alert('That is correct! Great Work!');
+    break;
+  } else if(numberGuess < correctAnswer) {
+    alert('Sorry! That number is too low. Try a higher number.');
+  } else if(numberGuess > correctAnswer) {
+    alert('Sorry! That number is too high. Try a lower number.');
+  }
+  if (i == '3') {
+    alert('I\'m sorry but that\'s the final guess. The correct answer was 12.');
+  }
 }
 
-//question 7
+//question 7 Pokemon question. Name one of the Kanto Starters (one of the original starter Pokemon from the original Pokemon Red, Green, Blue, Yellow games)
+
+var kantoStarter = ['bulbasaur', 'charmander', 'squirtle', 'pikachu'];
+
+for(var i = 0; i < 6; i++){
+  var starterGuess = prompt('Now let\'s play one more game! Can you tell me the name of one of the original starter Pokemon from the original Red, Green, Blue, or Yellow Version Pokemon games? You have six tries to get the right answer.');
+if (starterGuess.toLowerCase() === 'bulbasaur') {
+  alert('Correct! The three original starter Pokemon from the Kanto region are Bulbasaur, Charmander and Squirtle. Pikachu was also a starter option in Pokemon Yellow Version');
+  break;
+} else if (starterGuess.toLowerCase() === 'charmander') {
+  alert('Correct! The three original starter Pokemon from the Kanto region are Bulbasaur, Charmander and Squirtle. Pikachu was also a starter option in Pokemon Yellow Version');
+  break;
+} else if (starterGuess.toLowerCase() === 'squirtle') {
+  alert('Correct! The three original starter Pokemon from the Kanto region are Bulbasaur, Charmander and Squirtle. Pikachu was also a starter option in Pokemon Yellow Version');
+  break;
+} else if (starterGuess.toLowerCase() === 'pikachu') {
+  alert('Correct! The three original starter Pokemon from the Kanto region are Bulbasaur, Charmander and Squirtle. Pikachu was also a starter option in Pokemon Yellow Version');
+  break;
+} else {
+  alert('Sorry, please try again! Remember starter Pokemon always are either Grass, Fire or Water types.');
+} if (i == '5') {
+  alert('Sorry! The correct answers were Bulbasaur, Charmander and Squirtle, but Pikachu is also accemptable because it was the starter Pokemon from Yellow Version.')
+}
+}
+
+alert('Thank you so much for answering the questions ' + userName + '! It was nice to meet you. Continue now to learn a little bit about me on the rest of the site.');
